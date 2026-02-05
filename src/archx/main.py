0b6b80889b9803efb9eca1db3b9489d14751772e
daniel <daniel.pricd@ucf.edu>
2026-02-05 12:10:18 -0500

    add -d argument

--- src/archx/main.py
+++ src/archx/main.py
@@ -1,6 +1,7 @@
 import pyfiglet, argparse, time, os, sys
 import pandas as pd
 import subprocess
+import shutil
 
 from loguru import logger
 
@@ -33,6 +34,8 @@ def parse_commandline_args():
                         help = 'Path to checkpoint, which requires <.gt> format.')
     parser.add_argument('-l', '--log_level', type=str, default='INFO',
                         help = 'Level of logger.')
+    parser.add_argument('-d', '--delete', action='store_true', default=False,
+                        help = 'Delete run directory if it exists.')
     parser.add_argument('-s', '--save_yaml', action='store_true', default=False, help = 'Save yaml files in run directory.')
     parser.add_argument('-ireg', '--register_interface', action='store_true', default=False, help = 'Register a new interface.')
     parser.add_argument('-iureg', '--unregister_interface', action='store_true', default=False, help = 'Unregister a new interface.')
@@ -60,6 +63,12 @@ def parse_commandline_args():
 
 
 def main():
+    if args.delete:
+        # delete run directory if it exists
+        if os.path.isdir(args.run_dir):
+            shutil.rmtree(args.run_dir)
+            logger.info(f'Delete existing run directory <{args.run_dir}>.')
+
     args = parse_commandline_args()
     
     # set up banner
